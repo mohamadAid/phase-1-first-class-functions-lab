cdbf04d45f4208ef505286c899358040f68af61a
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-05-17 00:24:58 +0300

    Done with the Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arrayOfDrivers) => {
+    return [arrayOfDrivers[0], arrayOfDrivers[1]];
+  };
+
+  const returnLastTwoDrivers = (arrayOfDrivers) => {
+    return [arrayOfDrivers[arrayOfDrivers.length - 2], arrayOfDrivers[arrayOfDrivers.length - 1]];
+  };
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  const createFareMultiplier = (num1) => {
+    return (fare) => fare * num1 ;
+  };
+
+  const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, fun) => fun(arrayOfDrivers); 
